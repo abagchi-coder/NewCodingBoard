16441d2d0276ee94f87446a82ee35c727212aa19
abagchi-coder <73923270+abagchi-coder@users.noreply.github.com>
2023-02-21 13:22:13 +0530

    format corrected

--- increment.cpp
+++ increment.cpp
@@ -6,14 +6,14 @@ int main(){
     float new_sal;
     int cnt;
   
-    printf("Enter years of experience: ");
+    printf("Enter years of Experience: ");
     scanf("%d",&years);
-    printf("Enter start Sal: ");
+    printf("Enter Starting Salary: ");
     scanf("%f",&start_sal);
     
     new_sal = start_sal;
     for(cnt=1;cnt<=years;cnt++){
-        printf("Enter avreage hike for year : ",cnt);
+        printf("Enter avreage hike for year no- %d: ",cnt);
         scanf("%f",&avg_prcnt);
         
         new_sal = new_sal + new_sal*(avg_prcnt/100);
